12dd8a77707bfc2c4b09b5a664b46aca64d5ede8
Mohammad Za'areer <zaa92reer@gmail.com>
2021-05-20 02:13:01 +0300

    setting up repo

--- index.js
+++ index.js
@@ -3,9 +3,9 @@
 // Start up DB Server
 const mongoose = require('mongoose');
 const options = {
-  useNewUrlParser: true,
-  useCreateIndex: true,
-  useUnifiedTopology: true,
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
 };
 mongoose.connect(process.env.MONGODB_URI, options);
 
